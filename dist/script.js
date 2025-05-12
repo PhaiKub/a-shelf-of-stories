ee86e6253665a00d33f05b023625daf573c3f2dc
PhaiKub <phai251025@gmail.com>
2025-05-12 23:59:17 +0700

    mabey all of my

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,213 @@
-"use strict";
+const data = {
+  Reading: [
+    {
+      title: "I was Reincarnated as the Scumbag from a Netorare Manga, But the Heroine is Coming on to Me",
+      image: "https://img.dlsite.jp/modpub/images2/work/books/BJ01023000/BJ01022944_img_main.jpg",
+      color: "#b0b6a9"
+    },
+    {
+      title: "She is the Neighbor Angel I am Spoiled by Her",
+      image: "https://cdn.novelupdates.com/images/2020/04/Otonari-no-Tenshi-sama-ni-Itsu-no-Aida-ni-ka-Dame-Ningen-ni-Sareteiru-ken.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "Why Don't You Make the Twins Your 'Girlfriend' Together?",
+      image: "https://preview.redd.it/art-volume-1-cover-why-dont-you-make-the-twins-your-v0-v3dw9tsfqcjd1.jpeg?width=640&crop=smart&auto=webp&s=cbd30aa7af6fe2462ed510e3dc68dd16d6383b77",
+      color: "#afa294"
+    },
+    {
+      title: "I Tried to Stop Being the Last Boss ~I Pretended to be Defeated by the Main Character and Tried to Live Freely~",
+      image: "https://m.media-amazon.com/images/I/510n9wYgdyL._SY445_SX342_.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "The Eminence in Shadow",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRThiQD98KJNATVEeeq54ra_PnzLQrVYLhNxA&s",
+      color: "#afa294"
+    },
+    {
+      title: "Can a Boy and Girl Friendship Hold Up? (No It Can't)",
+      image: "https://external-preview.redd.it/CZxnXW3-Nyre7cS9qxaIvkLMxaNoWvjKJapSRMODgOY.jpg?width=640&crop=smart&auto=webp&s=845de7edb4a03e383df7c0d0d9baaf6bfa53a32e",
+      color: "#afa294"
+    },
+    {
+      title: "Shimotsuki-san Likes the Mob: This Shy Girl is Only Sweet Towards Me",
+      image: "https://m.media-amazon.com/images/I/51D7rAMtu9L._SY445_SX342_.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "The Ice Princess Wants to be Melted by a Small Patch of Sunlight",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAvy62zvu0Z_mvhkf1MkswSJsUjDfjtRnvCw&s",
+      color: "#afa294"
+    },
+    {
+      title: "An Introvert's Hookup Hiccups: This Gyaru Is Head Over Heels for Me!",
+      image: "https://m.media-amazon.com/images/I/81OOac4i7PL._UF1000,1000_QL80_.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "Too Many Losing Heroines!",
+      image: "https://upload.wikimedia.org/wikipedia/en/4/4c/Make_Heroine_ga_Oosugiru%21_light_novel_volume_1_cover.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "The Babe at My Back Has Her Eye on Me. I'm Done For",
+      image: "https://i0.wp.com/c.bookwalker.jp/coverImage_419356ff429b4aaea4255464a76cb252.jpg?ssl=1",
+      color: "#afa294"
+    },
+    {
+      title: "Since I’ve Entered the World of Romantic Comedy Manga, I’ll Do My Best to Make the Losing Heroine Happy",
+      image: "https://i.redd.it/p74t5cegwsnb1.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "Kusunoki's Flunking Her High School Glow-Up",
+      image: "https://cdn2.penguin.com.au/covers/original/9798888772720.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "When I Got to Remote Class, I had to Move in with the Most Beautiful Girl in My Class",
+      image: "https://www.osemocphoto.com/collectManga/14690/14690_cover.jpg?ver=1",
+      color: "#afa294"
+    },
+    {
+      title: "About That Time I Didn’t Want to Have a Matchmaking Interview, So I Set Some Impossible Conditions and Then My Classmate Showed Up",
+      image: "https://www.mgeko.cc/media/manga_covers/omiai-shitakunakatta-no-de-muri-nandai-na.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "To Become Your Ideal Maid!",
+      image: "https://www.osemocphoto.com/collectManga/13891/13891_cover.jpg?ver=3",
+      color: "#afa294"
+    },
+    {
+      title: "When I Brought a Lost Girl Home, the Most Beautiful Girl in My Grade Came Out the Front Door",
+      image: "https://cdn.novelupdates.com/images/2025/01/When-I-Brought-The-Lost-Little-Girl-To-Her-House-The-One-Who-Came-Out-Of-The-Front-Door-Was-The-Most-Beautiful-Girl-In-The-Grade.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "SSS-Class Suicide Hunter",
+      image: "https://preview.redd.it/raviel-ivansia-the-lady-silver-lily-of-the-sss-class-v0-cyl7qs4icmyc1.jpg?width=640&crop=smart&auto=webp&s=2466434dd7b269a8afaa94a4cdbde3959c70b04c",
+      color: "#afa294"
+    },
+    {
+      title: "Terminally-Ill Genius Dark Knight",
+      image: "https://cdn.novelupdates.com/images/2023/06/TerminallyIll-Genius-Dark-Knight.png",
+      color: "#afa294"
+    },
+    {
+      title: "The Extra's Academy Survival Guide",
+      image: "https://cdn.mangaupdates.com/image/i433221.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "Fake Girlfriend? My Fault?",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4C0QiZD9weU0RIwVqtAZbvD18Q6qdry3G3Q&s",
+      color: "#afa294"
+    },
+    {
+      title: "Shut Up, Evil Dragon! I don't want to raise a child with you anymore",
+      image: "https://image.readrealm.co/comics-cover/6013408e-0283-421e-8c78-7f3bbc8fe2d9.webp",
+      color: "#afa294"
+    }
+  ],
+  Completed: [
+    {
+      title: "Kaguya-sama wa Kokurasetai",
+      image: "https://i.redd.it/jy2erqldg33a1.jpg",
+      color: "#6e695e"
+    },
+    {
+      title: "Nisekoi",
+      image: "https://m.media-amazon.com/images/M/MV5BMzcxMjBiNWMtNDBmYi00NmFlLWFlOTctZjlkMzExMWU0YTI5XkEyXkFqcGc@._V1_.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "365 Days to the Wedding",
+      image: "https://upload.wikimedia.org/wikipedia/en/c/cd/Kekkon_Surutte%2C_Hont%C5%8D_desu_ka_1.png",
+      color: "#6e695e"
+    },
+    {
+      title: "Love form Home",
+      image: "https://www.osemocphoto.com/collectManga/14891/14891_cover.jpg?ver=1",
+      color: "#afa294"
+    },
+    {
+      title: "My Ex-Wife is Young Again and She's in my Class",
+      image: "https://static.usagi.one/uploads/pics/02/48/557.jpg",
+      color: "#afa294"
+    },
+    {
+      title: "Webtoon Character Na Kang Lim",
+      image: "https://i.namu.wiki/i/HiIb_pUxnwRp7yFxvgFch1ZQHSjBS7ztEU04va232KZMTnjny9VaMS3uqvlRAQN9ZwqgAlerBJHlt-K6YByCAA.webp",
+      color: "#afa294"
+    },
+    {
+      title: "Why is my Strict Boss Melted by Me?",
+      image: "https://www.bedetheque.com/media/Couvertures/Couv_495470.jpg",
+      color: "#afa294"
+    }
+  ],
+  Planning: [
+    {
+      title: "Tune In to the Midnight Heart",
+      image: "https://m.media-amazon.com/images/I/91K9WWl3esL._AC_UF1000,1000_QL80_.jpg",
+      color: "#7b4d35"
+    },
+    {
+      title: "A Villain's Will to Survive",
+      image: "https://preview.redd.it/a-villains-will-to-survive-is-this-really-worth-all-the-hype-v0-r4weqnv0keue1.png?width=1080&crop=smart&auto=webp&s=46a92c1e996283fd85e118ee985aac59401a83f4",
+      color: "#7b4d35"
+    },
+    {
+      title: "Playing the Perfect Fox-Eyed Villain",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWtzZ6s68v6XBI9IzFuASf51NRDf5yrHxCXQ&s",
+      color: "#7b4d35"
+    },
+    {
+      title: "Fated to Be Loved by Villains",
+      image: "https://i.namu.wiki/i/183LoPyzfAsqXufZ5nqB19075q32_f6ksR_pajsyoghlQ6ogw-RcMzarWSDOEMwQG_L7IVJPu82LFZMcolUZ8g.webp",
+      color: "#7b4d35"
+    },
+    {
+      title: "I’m a Hero, but the Heroines are Trying to Kill Me",
+      image: "https://kenmei.b-cdn.net/covers/manga_series/119944/cover/jpeg-thumbnail-large-696791e1aa15349a5e24894800d85ef9.jpeg",
+      color: "#7b4d35"
+    },
+    {
+      title: "Today is a woman day",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSokO9f5PFYZEQTS5UO0atb35ahishsNC9aew&s",
+      color: "#7b4d35"
+    },
+  ]
+};
+
+function createSection(title, items) {
+  const section = document.createElement("section");
+
+  const header = document.createElement("header");
+  header.innerHTML = `<h1>${title}</h1>`;
+  section.appendChild(header);
+
+  items.forEach(item => {
+    const article = document.createElement("article");
+    article.style.setProperty("--avarage-color", item.color);
+    article.innerHTML = `
+      <figure>
+        <img src="${item.image}" />
+        <figcaption>${item.title}</figcaption>
+      </figure>
+    `;
+    section.appendChild(article);
+  });
+
+  return section;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("shelf-container");
+  for (const [category, items] of Object.entries(data)) {
+    const section = createSection(category, items);
+    container.appendChild(section);
+  }
+});
