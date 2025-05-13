ab75bdd8b11482837c93719d72c54c60f41bb36b
PhaiKub <phai251025@gmail.com>
2025-05-13 16:04:44 +0700

    add bg and popup

--- script.js
+++ script.js
@@ -3,186 +3,222 @@ const data = {
     {
       title: "I was Reincarnated as the Scumbag from a Netorare Manga, But the Heroine is Coming on to Me",
       image: "https://img.dlsite.jp/modpub/images2/work/books/BJ01023000/BJ01022944_img_main.jpg",
-      color: "#b0b6a9"
+      color: "#b0b6a9",
+      backgroundImage:"https://preview.redd.it/i-was-reincarnated-as-the-scumbag-from-a-netorare-manga-but-v0-v8rf8dgaahgb1.jpg?auto=webp&s=b838f6b5e7d8f8a863f566c47d2f85fef9953967"
     },
     {
       title: "She is the Neighbor Angel I am Spoiled by Her",
       image: "https://cdn.novelupdates.com/images/2020/04/Otonari-no-Tenshi-sama-ni-Itsu-no-Aida-ni-ka-Dame-Ningen-ni-Sareteiru-ken.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://pbs.twimg.com/media/FIQxCpkVUAcu8eB?format=jpg&name=4096x4096"
     },
     {
       title: "Did You Think You Could Live Normally Even in a World With a 1:5 Male-To-Female Ratio? ~ When Deeply Intense Feelings of the Girls Are Toyed With by an Oblivious Boy",
       image: "https://i.namu.wiki/i/LcZ9GVow3c2jxQTDD-N04tsIPGxFt19C0t3-hRSqcjXGnL2rPf6n4ez-E6HFO19mhmkuhpctHUxOQcIfanDrbw.webp",
-      color: "#b0b6a9"
+      color: "#b0b6a9",
+      backgroundImage:"https://pbs.twimg.com/media/GbvkhmZXIAgD6BS?format=jpg&name=4096x4096"
     },
     {
       title: "Why Don't You Make the Twins Your 'Girlfriend' Together?",
       image: "https://preview.redd.it/art-volume-1-cover-why-dont-you-make-the-twins-your-v0-v3dw9tsfqcjd1.jpeg?width=640&crop=smart&auto=webp&s=cbd30aa7af6fe2462ed510e3dc68dd16d6383b77",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://preview.redd.it/sl-request-why-dont-you-make-the-twins-your-girlfriend-v0-je4zh4vunqwc1.png?width=503&format=png&auto=webp&s=2d5167681b5a548d8f9a159a11484d8f62dd56bf"
     },
     {
       title: "I Tried to Stop Being the Last Boss ~I Pretended to be Defeated by the Main Character and Tried to Live Freely~",
       image: "https://m.media-amazon.com/images/I/510n9wYgdyL._SY445_SX342_.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://pbs.twimg.com/media/GqtPZhQWsAA6IHm.jpg:large"
     },
     {
       title: "The Eminence in Shadow",
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRThiQD98KJNATVEeeq54ra_PnzLQrVYLhNxA&s",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://musicart.xboxlive.com/7/db236400-0000-0000-0000-000000000002/504/image.jpg"
     },
     {
       title: "Can a Boy and Girl Friendship Hold Up? (No It Can't)",
       image: "https://external-preview.redd.it/CZxnXW3-Nyre7cS9qxaIvkLMxaNoWvjKJapSRMODgOY.jpg?width=640&crop=smart&auto=webp&s=845de7edb4a03e383df7c0d0d9baaf6bfa53a32e",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://gamicsoft.sgp1.digitaloceanspaces.com/32932/conversions/boygirl-big_thumb.jpg"
     },
     {
       title: "Shimotsuki-san Likes the Mob: This Shy Girl is Only Sweet Towards Me",
       image: "https://m.media-amazon.com/images/I/51D7rAMtu9L._SY445_SX342_.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://cdn3.mangaclash.com/shimotsuki-san-likes-the-mob-this-shy-girl-is-only-sweet-towards-me-40520/chapter-1/3.jpg"
     },
     {
-      title: "The Ice Princess Wants to be Melted by a Small Patch of Sunlight",
-      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAvy62zvu0Z_mvhkf1MkswSJsUjDfjtRnvCw&s",
-      color: "#afa294"
+      title: "After Helping the 'Ice Princess' From Another School, I Decided to Start as a Friend",
+      image: "https://cdn.novelupdates.com/images/2024/05/After-I-Save-The-Ice-Princess-From-Another-School-From-a-Molster-We-Started-as-Friends.jpg",
+      color: "#afa294",
+      backgroundImage:"https://preview.redd.it/disc-after-helping-the-ice-princess-from-another-school-i-v0-edkypwa2caje1.jpg?width=640&crop=smart&auto=webp&s=93ca6cbb4cc1c43da3c40e83e9649357d5e5d6e7"
     },
     {
       title: "An Introvert's Hookup Hiccups: This Gyaru Is Head Over Heels for Me!",
       image: "https://m.media-amazon.com/images/I/81OOac4i7PL._UF1000,1000_QL80_.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://pbs.twimg.com/media/F8a3pvja0AALSJM.jpg"
     },
     {
       title: "Too Many Losing Heroines!",
       image: "https://upload.wikimedia.org/wikipedia/en/4/4c/Make_Heroine_ga_Oosugiru%21_light_novel_volume_1_cover.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://musicart.xboxlive.com/7/76086c00-0000-0000-0000-000000000002/504/image.jpg"
     },
     {
       title: "The Babe at My Back Has Her Eye on Me. I'm Done For",
       image: "https://i0.wp.com/c.bookwalker.jp/coverImage_419356ff429b4aaea4255464a76cb252.jpg?ssl=1",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://scontent.fphs2-1.fna.fbcdn.net/v/t39.30808-6/486188893_693027583280847_3486680590607477209_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeGn-EWqjWikmNXOhKKv6NbtIKwGr3SrBZggrAavdKsFmPvv1fJ4_hQfPcBGx3kPNA3Ub_g6-JXLRrd0qjZM57g0&_nc_ohc=9v0LrzU6R7IQ7kNvwGNo3_o&_nc_oc=AdmcOW16ek8mqKQ7ea7jE-0Va8KRGJL_7kiv4yNxf9fRK_ew5pimfVKKLVxOYEKIwm3EJWNX2tpVPmPeX4ZH5kpY&_nc_zt=23&_nc_ht=scontent.fphs2-1.fna&_nc_gid=kCwJAoMk7iH114moKfVzAA&oh=00_AfKeQW9jjsV2DoFDhoYSs5dHli8h5Yh4RqfdI_el2h7XSg&oe=6828C4D9"
     },
     {
       title: "Since I’ve Entered the World of Romantic Comedy Manga, I’ll Do My Best to Make the Losing Heroine Happy",
       image: "https://i.redd.it/p74t5cegwsnb1.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://external-preview.redd.it/news-since-ive-entered-the-world-of-romantic-comedy-manga-v0-yvGwGs-N2gwA-cuHJtgCwLBkGB9IbukcFmijjicuX9M.jpg?auto=webp&s=af6d42361e9446be4996a4c0ccd7029ab5350ebc"
     },
     {
       title: "Kusunoki's Flunking Her High School Glow-Up",
       image: "https://cdn2.penguin.com.au/covers/original/9798888772720.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://preview.redd.it/kusunoki-san-2025-new-year-illustration-v0-fx8m7dqb0bae1.jpeg?auto=webp&s=c9164cbce625803521317264165345e7f1a47bf1"
     },
     {
       title: "When I Got to Remote Class, I had to Move in with the Most Beautiful Girl in My Class",
       image: "https://www.osemocphoto.com/collectManga/14690/14690_cover.jpg?ver=1",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://preview.redd.it/art-remote-jugyou-ni-nattara-class-ichi-no-bishoujo-to-v0-xvywdr4z0wm81.jpg?width=640&crop=smart&auto=webp&s=e08a32be6d09fffde8ef06161f13dab062950f77"
     },
     {
       title: "About That Time I Didn’t Want to Have a Matchmaking Interview, So I Set Some Impossible Conditions and Then My Classmate Showed Up",
       image: "https://www.mgeko.cc/media/manga_covers/omiai-shitakunakatta-no-de-muri-nandai-na.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://external-preview.redd.it/B7TDU9GRg1NCwC8yyYj__ZKRQ1GPZ5b595Tg_8K4orw.jpg?width=640&crop=smart&auto=webp&s=de77b56bdd3f893877f1c123f2616c8947a03c4e"
     },
     {
       title: "To Become Your Ideal Maid!",
       image: "https://www.osemocphoto.com/collectManga/13891/13891_cover.jpg?ver=3",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://static.mangafire.to/82fb/i/5/56/3178e727eaa9b03ebc3300c07e8f382c.jpg"
     },
     {
       title: "When I Brought a Lost Girl Home, the Most Beautiful Girl in My Grade Came Out the Front Door",
       image: "https://cdn.novelupdates.com/images/2025/01/When-I-Brought-The-Lost-Little-Girl-To-Her-House-The-One-Who-Came-Out-Of-The-Front-Door-Was-The-Most-Beautiful-Girl-In-The-Grade.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://i2.wp.com/zetrotranslation.com/wp-content/uploads/2023/08/peakpx-4.jpg"
     },
     {
       title: "SSS-Class Suicide Hunter",
       image: "https://preview.redd.it/raviel-ivansia-the-lady-silver-lily-of-the-sss-class-v0-cyl7qs4icmyc1.jpg?width=640&crop=smart&auto=webp&s=2466434dd7b269a8afaa94a4cdbde3959c70b04c",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://i.pinimg.com/736x/99/26/fd/9926fdc8db807e4583e3a6d08ced550f.jpg"
     },
     {
       title: "Terminally-Ill Genius Dark Knight",
       image: "https://cdn.novelupdates.com/images/2023/06/TerminallyIll-Genius-Dark-Knight.png",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://preview.redd.it/terminally-ill-genius-dark-knight-am-i-seeing-double-v0-dyoj7f47k86c1.jpeg?auto=webp&s=e3be9a8342d812bf47118ab36ff6ced8bf435334"
     },
     {
       title: "The Extra's Academy Survival Guide",
       image: "https://cdn.mangaupdates.com/image/i433221.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://i.pinimg.com/736x/a5/a3/ae/a5a3ae49329276d7c58b0416d1d82e46.jpg"
     },
     {
       title: "Fake Girlfriend? My Fault?",
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4C0QiZD9weU0RIwVqtAZbvD18Q6qdry3G3Q&s",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://img.inu-manga.com/1e/3a/31/SFMDRE7ZA9-1652250099.jpg"
     },
     {
       title: "Shut Up, Evil Dragon! I don't want to raise a child with you anymore",
       image: "https://image.readrealm.co/comics-cover/6013408e-0283-421e-8c78-7f3bbc8fe2d9.webp",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://i.pinimg.com/736x/d9/61/79/d961799bf729633007c349de8ad518c1.jpg"
     }
   ],
   Completed: [
     {
       title: "Kaguya-sama wa Kokurasetai",
       image: "https://i.redd.it/jy2erqldg33a1.jpg",
-      color: "#6e695e"
+      color: "#6e695e",
+      backgroundImage:"https://wallpapers.com/images/high/kaguya-sama-love-is-war-r3q86urkqdowbwxq.webp"
     },
     {
       title: "Nisekoi",
       image: "https://m.media-amazon.com/images/M/MV5BMzcxMjBiNWMtNDBmYi00NmFlLWFlOTctZjlkMzExMWU0YTI5XkEyXkFqcGc@._V1_.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://preview.redd.it/t5zlwnq0zjoe1.jpeg?auto=webp&s=b2d0a904c7e9f56c8ebbcc8121608fd91e232382"
     },
     {
       title: "365 Days to the Wedding",
       image: "https://upload.wikimedia.org/wikipedia/en/c/cd/Kekkon_Surutte%2C_Hont%C5%8D_desu_ka_1.png",
-      color: "#6e695e"
-    },
-    {
-      title: "Love form Home",
-      image: "https://www.osemocphoto.com/collectManga/14891/14891_cover.jpg?ver=1",
-      color: "#afa294"
-    },
-    {
-      title: "My Ex-Wife is Young Again and She's in my Class",
-      image: "https://static.usagi.one/uploads/pics/02/48/557.jpg",
-      color: "#afa294"
-    },
-    {
-      title: "Webtoon Character Na Kang Lim",
-      image: "https://i.namu.wiki/i/HiIb_pUxnwRp7yFxvgFch1ZQHSjBS7ztEU04va232KZMTnjny9VaMS3uqvlRAQN9ZwqgAlerBJHlt-K6YByCAA.webp",
-      color: "#afa294"
+      color: "#6e695e",
+      backgroundImage:"https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/4c400d4323ea9b8b6487403a165ca8ec.jpg"
     },
     {
       title: "Why is my Strict Boss Melted by Me?",
       image: "https://www.bedetheque.com/media/Couvertures/Couv_495470.jpg",
-      color: "#afa294"
+      color: "#afa294",
+      backgroundImage:"https://pbs.twimg.com/media/FJNmYjbaAAQ7dAZ.jpg:large"
+    },
+    {
+      title: "Love form Home",
+      image: "https://www.osemocphoto.com/collectManga/14891/14891_cover.jpg?ver=1",
+      color: "#afa294",
+      backgroundImage:"https://otakuusamagazine.com/wp-content/uploads/2024/10/ousa_home_hero.png"
+    },
+    {
+      title: "My Ex-Wife is Young Again and She's in my Class",
+      image: "https://static.usagi.one/uploads/pics/02/48/557.jpg",
+      color: "#afa294",
+      backgroundImage:"https://pbs.twimg.com/media/FSGGlJSaUAAPUJY.jpg"
+    },
+    {
+      title: "Webtoon Character Na Kang Lim",
+      image: "https://i.namu.wiki/i/HiIb_pUxnwRp7yFxvgFch1ZQHSjBS7ztEU04va232KZMTnjny9VaMS3uqvlRAQN9ZwqgAlerBJHlt-K6YByCAA.webp",
+      color: "#afa294",
+      backgroundImage:"https://pbs.twimg.com/media/GSoRfqvbIAA_xum?format=jpg&name=large"
     }
   ],
   Planning: [
     {
       title: "Tune In to the Midnight Heart",
       image: "https://m.media-amazon.com/images/I/91K9WWl3esL._AC_UF1000,1000_QL80_.jpg",
-      color: "#7b4d35"
+      color: "#7b4d35",
+      backgroundImage:"https://preview.redd.it/for-some-reason-i-have-a-feeling-that-either-rikka-nene-or-v0-z4ldejrmfc0f1.png?width=640&crop=smart&auto=webp&s=615150ccea70be2a4c46635e1040207b3dbf6714"
     },
     {
       title: "A Villain's Will to Survive",
       image: "https://preview.redd.it/a-villains-will-to-survive-is-this-really-worth-all-the-hype-v0-r4weqnv0keue1.png?width=1080&crop=smart&auto=webp&s=46a92c1e996283fd85e118ee985aac59401a83f4",
-      color: "#7b4d35"
+      color: "#7b4d35",
+      backgroundImage:"https://s1.zerochan.net/A.Villain%E2%80%99s.Will.to.Survive.600.4425625.jpg"
     },
     {
       title: "Playing the Perfect Fox-Eyed Villain",
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWtzZ6s68v6XBI9IzFuASf51NRDf5yrHxCXQ&s",
-      color: "#7b4d35"
+      color: "#7b4d35",
+      backgroundImage:"https://scontent.fphs2-1.fna.fbcdn.net/v/t39.30808-6/486645659_122217431276079743_1320543241170454460_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeHPENzvoxc_bSy1coCUFCSYEEcWk_KwJQwQRxaT8rAlDAMjL4NX4fFZkyy7fviBXdxZy9WmrCQjQ8iol-895BPN&_nc_ohc=hyrw5SdVSnkQ7kNvwF6oa-K&_nc_oc=AdmKDa26_NsOLgQIULl_99bPLnt9o28txOFQtp5cD2ukUctRlAIvM-MjahBFiZ5FB_wiCzj3Gm8r_x_10wQBAyVg&_nc_zt=23&_nc_ht=scontent.fphs2-1.fna&_nc_gid=poC8A64qFtqAWFpGu4qD9w&oh=00_AfJBoelWhCk5fJ0ze9dZyY9NsEINV8Ya79oF3W_0JjfwqQ&oe=6828C8D3"
     },
     {
       title: "Fated to Be Loved by Villains",
       image: "https://i.namu.wiki/i/183LoPyzfAsqXufZ5nqB19075q32_f6ksR_pajsyoghlQ6ogw-RcMzarWSDOEMwQG_L7IVJPu82LFZMcolUZ8g.webp",
-      color: "#7b4d35"
+      color: "#7b4d35",
+      backgroundImage:"https://i.namu.wiki/i/FEhgYjNgZJxGBWPIqPtrIs-HBPxr1QvJdz2Ehk03OKkj2xwLmJMKYbKPt8-auJgEvVEM_-ZRfBlrGprx1eKiWQ.webp"
     },
     {
       title: "I’m a Hero, but the Heroines are Trying to Kill Me",
       image: "https://kenmei.b-cdn.net/covers/manga_series/119944/cover/jpeg-thumbnail-large-696791e1aa15349a5e24894800d85ef9.jpeg",
-      color: "#7b4d35"
+      color: "#7b4d35",
+      backgroundImage:"https://i.namu.wiki/i/sCURm7Np4I0XO9Gj8rbVY06BLWfqbT_67DHHtqZktFnttCDKPUdO8PaRfa1g0MrFJTGMiBGmZx6r5lIOHQMitg.webp"
     },
     {
       title: "Today is a woman day",
       image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSokO9f5PFYZEQTS5UO0atb35ahishsNC9aew&s",
-      color: "#7b4d35"
+      color: "#7b4d35",
+      backgroundImage:"https://kenmei.b-cdn.net/covers/manga_series/122792/cover/open_graph-03c2888ab77efc353482ce7b63c35b83.jpeg"
     },
   ]
 };
@@ -204,6 +240,26 @@ function createSection(title, items) {
       </figure>
     `;
     section.appendChild(article);
+    
+    // เพิ่ม event listener ให้กับ article
+    article.addEventListener("click", () => {
+      const popup = document.getElementById("popup-modal");
+      const popupImage = document.getElementById("popup-image");
+      const popupTitle = document.getElementById("popup-title");
+      const popupLeft = document.querySelector(".popup-left");
+
+      popupImage.src = item.backgroundImage;
+      popupTitle.textContent = item.title;
+
+      // ตรวจสอบว่า backgroundImage มีค่าหรือไม่ และตั้งค่าให้กับ popup
+      if (item.backgroundImage) {
+        popupLeft.style.backgroundImage = `url(${item.backgroundImage})`;
+      } else {
+        popupLeft.style.backgroundImage = ""; // ถ้าไม่มี backgroundImage กำหนดเป็นค่าว่าง
+      }
+
+      popup.classList.remove("hidden");
+    });
   });
 
   return section;
@@ -211,8 +267,19 @@ function createSection(title, items) {
 
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("shelf-container");
+
+  // สร้าง section และเพิ่มเข้าไปใน container
   for (const [category, items] of Object.entries(data)) {
     const section = createSection(category, items);
     container.appendChild(section);
   }
+
+  // เพิ่มโค้ดสำหรับปิด popup
+  const popup = document.getElementById("popup-modal");
+  const closeBtn = document.getElementById("close-popup"); // เพิ่มตัวเลือกสำหรับปุ่มปิด
+  popup.addEventListener("click", (e) => {
+    if (e.target === popup || e.target === closeBtn) {
+      popup.classList.add("hidden");
+    }
+  });
 });
